602d8556cb67ab0ff8873b639d6ac1109ef1126a
Ionut Marin <ionut_marinn@yahoo.com>
2018-01-24 21:56:28 +0200

    added final project zip

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = config;
 config.DB = {};
 config.DB.host = 'localhost';
 config.DB.user = 'root';
-config.DB.password = '';
+config.DB.password = 'root';
 config.DB.database = 'big_data_final_project';
